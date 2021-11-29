1ea76fda66102d19ad120fe65559280a8c1172bd
Manojkc15 <manojkc9845@gmail.com>
2021-11-29 20:07:12 +0530

    appbar implementation

--- lib/Screens/discover_screen.dart
+++ lib/Screens/discover_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
@@ -6,7 +7,32 @@ class DiscoverScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Discover",
+          style: GoogleFonts.sourceSerifPro(
+            fontSize: 27.0,
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+          size: 28.0,
+        ),
+        actions: [
+          Container(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: const CircleAvatar(
+              backgroundImage: AssetImage("assets/images/avatar.png"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
