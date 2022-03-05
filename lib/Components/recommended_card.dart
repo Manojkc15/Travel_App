3d71d946136849e1bd784e430b51eb130e70ef16
Manojkc15 <manojkc9845@gmail.com>
2022-03-06 00:46:51 +0530

    major update on gradle files, fixes internal code structure

--- lib/Components/recommended_card.dart
+++ lib/Components/recommended_card.dart
@@ -20,75 +20,78 @@ class RecommendedCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.fromLTRB(11.0, 0.0, 11.0, 0.0),
-          height: 170.0,
-          width: MediaQuery.of(context).size.width / 2.24,
-          decoration: BoxDecoration(
-            color: const Color(0xff7c94b6),
-            image: DecorationImage(
-              image: image,
-              fit: BoxFit.cover,
-            ),
-            borderRadius: BorderRadius.circular(12.0),
-          ),
+    // return Stack(
+    // children: [
+    return Container(
+      margin: const EdgeInsets.fromLTRB(11.0, 0.0, 11.0, 0.0),
+      height: 170.0,
+      width: MediaQuery.of(context).size.width / 2.24,
+      decoration: BoxDecoration(
+        color: const Color(0xff7c94b6),
+        image: DecorationImage(
+          image: image,
+          fit: BoxFit.cover,
         ),
-        Container(
-          width: MediaQuery.of(context).size.width / 2.24,
-          margin: const EdgeInsets.only(left: 12.0, top: 123.0),
-          padding: const EdgeInsets.fromLTRB(8.0, 5.0, 0.0, 5.0),
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(12),
-              bottomRight: Radius.circular(12),
-              topLeft: Radius.circular(8),
-              topRight: Radius.circular(8),
-            ),
-            color: Colors.black.withOpacity(0.3),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2.24,
+        margin: const EdgeInsets.only(top: 113.0),
+        padding: const EdgeInsets.fromLTRB(8.0, 5.0, 0.0, 5.0),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(12),
+            bottomRight: Radius.circular(12),
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: GoogleFonts.merriweather(
-                  fontSize: 14.5,
-                  color: Colors.white,
-                ),
-              ),
-              Row(
-                children: [
-                  stars,
-                  const SizedBox(width: 3),
-                  Text(
-                    rating,
-                    style: GoogleFonts.roboto(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 15.0,
-                    ),
+          color: Colors.black.withOpacity(0.3),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: GoogleFonts.merriweather(
+                    fontSize: 14.5,
+                    color: Colors.white,
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(7.0),
+                  margin: const EdgeInsets.only(right: 7.0, top: 3.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white,
+                  ),
+                  child: const Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                    size: 12.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                stars,
+                const SizedBox(width: 3),
+                Text(
+                  rating,
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-        Container(
-          padding: const EdgeInsets.all(7.0),
-          margin: const EdgeInsets.only(left: 162.0, top: 112.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30.0),
-            color: Colors.white,
-          ),
-          child: const Icon(
-            Icons.favorite,
-            color: Colors.red,
-            size: 12.0,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
